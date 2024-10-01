22767918c06e37ba58fd11d2a8a3261f23d8298e
Sara-Maude <sara-maude.laliberte@polymtl.ca>
2024-10-01 12:11:41 -0400

    version SM

--- TP2.py
+++ TP2.py
@@ -1,9 +1,9 @@
 """
 TP2 : Système de gestion de livres pour une bibliothèque
 
-Groupe de laboratoire : XX
-Numéro d'équipe :  YY
-Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
+Groupe de laboratoire : 01
+Numéro d'équipe : 19
+Noms et matricules : Virginie Cloutier (2373838), Sara-Maude Laliberté (2384032)
 """
 
 ########################################################################################################## 
@@ -11,10 +11,29 @@ Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
+import csv
+
+csvfile = open('collection_bibliotheque.csv', newline='')
 
 
+c = csv.reader(csvfile)
 
+bibliotheque = {}
 
+for row in c:
+    if row[0] == "titre":
+        continue
+    else:
+        titre = row[0]
+        auteur = row[1]
+        date_publication = row[2]
+        cote = row[3]
+
+        bibliotheque[cote] = {"titre" : titre, "auteur" : auteur, "date_publication" : date_publication}
+
+print(f' \n Bibliotheque initiale : {bibliotheque} \n')
+
+csvfile.close()
 
 
 ########################################################################################################## 
@@ -22,8 +41,25 @@ Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
+csvfile = open('nouvelle_collection.csv', newline='')
 
+c = csv.reader(csvfile)
 
+for row in c:
+    if row[0] == "titre":
+        continue
+    else:
+        titre = row[0]
+        auteur = row[1]
+        date_publication = row[2]
+        cote = row[3]
+
+        if cote in bibliotheque:
+            print(f"Le livre {cote} ---- {titre} par {auteur} ---- est déjà présent dans la bibliothèque")
+
+        else:
+            print(f"Le livre {cote} ---- {titre} par {auteur} ---- a été ajouté avec succès")
+            bibliotheque[cote] = {"titre" : titre, "auteur" : auteur, "date_publication" : date_publication}
 
 
 
@@ -33,22 +69,45 @@ Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
+new_bibliotheque = {}
+for cote in bibliotheque:
+    titre = bibliotheque[cote]["titre"]
+    auteur = bibliotheque[cote]["auteur"]
+    date_publication = bibliotheque[cote]["date_publication"]
+    if bibliotheque[cote]["auteur"] == "William Shakespeare":
+
+            cote = f"SW{cote[1:]}"
+            new_bibliotheque[cote] = {"titre" : titre, "auteur" : auteur, "date_publication" : date_publication}
+
+    else:
+        new_bibliotheque[cote] = {"titre": titre, "auteur": auteur, "date_publication": date_publication}
+
+
+bibliotheque = new_bibliotheque
 
 
 
-
-
-
+print(f' \n Bibliotheque avec modifications de cote : {bibliotheque} \n')
 
 ########################################################################################################## 
 # PARTIE 4 : Emprunts et retours de livres
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
+csvfile = open('emprunts.csv', newline='')
 
 
+c = csv.reader(csvfile)
+for cote in bibliotheque:
+        bibliotheque[cote]["emprunts"] = "disponible"
+
+for row in c:
+    if row[0] in bibliotheque:
+        bibliotheque[row[0]]["emprunts"] = "emprunté"
+        bibliotheque[row[0]]["date_emprunt"] = row[1]
 
 
+print(f' \n Bibliotheque avec ajout des emprunts : {bibliotheque} \n')
 
 
 
@@ -57,6 +116,43 @@ Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
+import datetime
+
+livre_retard = []
+livres_perdus = []
+for cote in bibliotheque:
+
+    if bibliotheque[cote]["emprunts"] == "emprunté":
+        date_emprunt = bibliotheque[cote]["date_emprunt"]
+        date_emprunt = date_emprunt.split("-")
+        annee = int(date_emprunt[0])
+        mois = int(date_emprunt[1])
+        jour = int(date_emprunt[2])
+
+        date_emprunt = datetime.datetime(annee, mois, jour)
+
+        date_actuelle = datetime.datetime.now()
+
+        temps_ecoule = (date_actuelle - date_emprunt).days
+
+        if temps_ecoule > 30:
+            frais_retard = (temps_ecoule - 30)*2
+
+            if frais_retard > 100:
+                frais_retard = 100
+
+            livre_retard.append(f"{bibliotheque[cote]['titre']} : {frais_retard}$")
+            bibliotheque[cote]["frais_retard"] = f"{frais_retard}$"
+
+        if temps_ecoule > 365:
+            livres_perdus.append(bibliotheque[cote]["titre"])
+            bibliotheque[cote]["livres_perdus"] = "livre perdu"
+
+print(f"liste des livres en retard avec les frais applicables: {livre_retard}")
+print(f"liste des livres perdus: {livres_perdus}")
+
+print(f' \n Bibliotheque avec ajout des retards et frais : {bibliotheque} \n')
+
 
 
 
